1302081c8ec160471f09b64fceae8ca955edb5fc
DanielMeP <75695953+DanielMeP@users.noreply.github.com>
2022-09-18 18:32:46 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,10 +1,45 @@
 #include "stats.h"
+#ifdef NAN
+/* NAN is supported */
+#endif
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+struct Stats {
+    float average;
+    float min;
+    float max;
+};
+
+struct Stats compute_statistics(const float* numberset, int setlength)
+{
+    int i;
+    float sum;
+    struct Stats S;
+    max_num = min_num = numberset [0];
+    
+    if(setlength == 0)
+    {
+        S = NAN
+    }else
+    {
+        for(i = 0; i <= setlength; i++)
+        {
+            sum += numberset[i];
+            if(max_num > numberset[i])
+            {
+                max_num = numberset[i];
+            }
+             if(min_num < numberset[i])
+            {
+                min_num = numberset[i];
+            }       
+        }
+
+        S.average = sum/setlength;
+        S.min = min_num;
+        S.max = max_num;
+    }
+    
+    return S;
 }
 
 int emailAlertCallCount = 0;
