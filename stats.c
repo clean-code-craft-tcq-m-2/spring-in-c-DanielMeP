853561c90672216438259b19c5d5c7e094ebc8e2
DanielMeP <75695953+DanielMeP@users.noreply.github.com>
2022-09-19 14:40:40 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -31,7 +31,7 @@ struct Stats compute_statistics(const float* numberset, int setlength)
             }       
             numberset++;
         }
-        sum = floor(sum * 1000)/1000;
+        printf("%f\n", sum );
         S.average = sum/setlength;
         S.min = min_num;
         S.max = max_num;
